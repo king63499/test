c67b3d7a2c6324c1e2fa54c41ef0ea8d5d853159
金昌礼 <jinchangli@xin.com>
2016-11-04 14:23:03 +0800

    [提交文件]

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+	$file = file("/Users/jinchangli/b.txt");
+	$i = 0;
+	echo '$data = ['."\n";
+	$str='';
+	$b = [
+		1 => 'yxp_order_createtime',
+		2 => 'dispose_time',
+		3 => 'yxp_transfer_localtime',
+		4 => 'dispose_price',
+		5 => 'dispose_transfer_fee',
+		6 => 'dispose_trade_fee',
+		7 => 'transfer_deposit_fee',
+	];
+	foreach($file as $line){
+		$line = trim($line);
+		
+		if(preg_match('/^[0-9]{4}\/[0-9]{1,2}\/[0-9]{1,2}\s+[0-9]{1,2}:[0-9]{1,2}$/',$line) != 0){
+			$line = date('Y-m-d H:i:s',strtotime($line));
+		}
+
+		if($i%8 == 0){
+			if(preg_match('/^\s*$/', $line) == 0){
+				$str .="\t". "'".$line."'".'=> [';
+			}else{
+				break;
+			}			
+		}else{
+			$k = $i%8;
+			if(($i+1)%8 == 0){
+				$str .="'".$b[$k]."' =>". "'".$line."'],\n";
+			}else{
+				$str .= "'".$b[$k]."' =>"."'".$line."'".',';
+			}
+		}
+		$i++;	
+	}
+	echo $str;
+	echo ']';
+
+?>
+
